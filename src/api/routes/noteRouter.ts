1f319a77440dc3bc3d89e77403b97e0bd0590de3
Adam Makuchowski <maku971@gmail.com>
2024-01-10 07:12:29 +0100

    update CORS config

--- src/api/routes/noteRouter.ts
+++ src/api/routes/noteRouter.ts
@@ -10,7 +10,7 @@ import {
 const router = express.Router()
 
 router.get(
-  '/getNote',
+  '/getNote/:id',
   withAsyncHandler(getNote)
 )
 
--- src/configs/corsConfig.ts
+++ src/configs/corsConfig.ts
@@ -6,7 +6,7 @@ interface CorsOptions {
 }
 
 const corsOptions: CorsOptions = {
-  origin: ['http://localhost:3000'],
+  origin: ['http://localhost:3000', 'http://localhost:5173'],
   optionsSuccessStatus: 200,
   allowedHeaders: ['Authorization', 'Content-Type'],
   methods: 'GET,POST,PUT,DELETE'
